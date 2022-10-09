1e7c48b06c4d706c983e4f1932954d96a799da53
bilal karahan <bilalkarahan309@gmail.com>
2022-10-09 14:51:06 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+    static boolean isPalindrome(String word){
+        int i=0,j=word.length()-1;
+        while (i<j){
+            if (word.charAt(i)!=word.charAt(j))
+                return false;
+        i++;j--;
+        }
+        return true;
+    }
+    static boolean isPalindrome2(String word){
+        String reverse="";
+        for (int i =word.length()-1; i >=0 ; i--) {
+            reverse+=word.charAt(i);
+        }
+        return word.equals(reverse);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      System.out.println(isPalindrome2("ccb"));
+      System.out.println(isPalindrome("ccb"));
     }
 }
